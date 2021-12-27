fb3e224bd3a4bf0f40264f25a21dce2c7aa8b6cb
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2021-12-27 21:31:56 +0200

    Clean Up temporary runtime files

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 #source /mnt/apps/prebuilt/eda/cadence-2019-20.bash
 
-ls ./hdl/*.v | xargs -n 1 basename > hdl.list
+ls ./hdl/*.v | xargs -n 1 basename > hdl.list.tmp
 
 mkdir ./exports
 rm -rf ./exports/*
@@ -16,3 +16,7 @@ mkdir ./tmp
 rm -rf ./tmp/*
 
 genus -f ./runtime/genus_exec.tcl
+
+rm -rf ./**.tmp
+rm -rf ./exports/*
+rm -rf ./tmp/*
